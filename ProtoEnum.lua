2251c6e66b3f99bb5b12644fe55b29c18e06f897
bruce <hanyongtao@live.com>
2020-06-05 20:35:42 +0800

    commit

---
+++ ProtoEnum.lua
@@ -0,0 +1,28 @@
+---@class State
+local State = {
+    Ok = 0,
+    Dead = 1,
+    Home = 2,
+}
+
+---@class State2
+local State2 = {
+    Ok = 0,
+    Dead = 1,
+    Home = 2,
+}
+
+---@class Enum1
+local Enum1 = {
+    A = 0,
+    B = 1,
+    C = 2,
+}
+
+
+
+return {
+    State=State,
+    State2=State2,
+    Enum1=Enum1,
+}
---
+++ ProtoIntelliSense.lua
@@ -0,0 +1,21 @@
+---@class Item
+---@field public id integer @道具ID
+---@field public count integer @道具数量
+local Item = {}
+print(Item)
+
+---@class One
+---@field public id integer @some id
+---@field public hello string @hello
+local One = {}
+print(One)
+
+---@class Player
+---@field public uid integer @玩家唯一id
+---@field public name string @玩家名字
+---@field public luckyrate number @玩家幸运值
+---@field public ok boolean
+---@field public items Item[] @玩家道具列表
+local Player = {}
+print(Player)
+
